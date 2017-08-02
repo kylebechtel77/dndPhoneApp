37f09bed5977e684b3c874210b018d6e44a848e5
Timothy Sassone <timothy.s.dev@gmail.com>
2017-08-01 20:00:12 -0400

    Fixed toolbar overlapping app.

--- App.js
+++ App.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 
 import AbilitiesScreen from './Screens/Abilities';
 import SkillsScreen from './Screens/Skills';
+import BaseStyleSheet from './Styles/styles';
 
- const App = TabNavigator ({
-   Abilities: { screen: AbilitiesScreen },
-     Skills: { screen: SkillsScreen },
- });
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 9,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    paddingTop: 20,
+const App = TabNavigator({
+  Abilities: { screen: AbilitiesScreen },
+  Skills: { screen: SkillsScreen },
+}, {
+  tabBarOptions: {
+    style: BaseStyleSheet.tabBar,
   },
 });
 
 export default () => (
-    <App />
+  <App />
 );
