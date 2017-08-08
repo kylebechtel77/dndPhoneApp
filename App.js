fe49927e95fbd3cc0fb4a2aec7bec66a35f69b8c
kylebechtel <kylebechtel@bellsouth.net>
2017-08-08 18:58:53 -0400

    missed an import

--- App.js
+++ App.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { TabNavigator } from 'react-navigation';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import BaseStyleSheet from './Styles/Base';
 import GeneralInfo from './Screens/GeneralInfo';
 import AbilitiesScreen from './Screens/Abilities';
 import SkillsScreen from './Screens/Skills';
-import dndApp from './Reducers'
+import dndApp from './Reducers/Reducer';
 
-let store = createStore(dndApp)
+const store = createStore(dndApp);
 
-const App = TabNavigator({
-  'General Info': { screen: GeneralInfo },
-  Abilities: { screen: AbilitiesScreen },
-  Skills: { screen: SkillsScreen },
-}, {
-  tabBarOptions: {
-    style: BaseStyleSheet.tabBar,
+const App = TabNavigator(
+  {
+    'General Info': { screen: GeneralInfo },
+    Abilities: { screen: AbilitiesScreen },
+    Skills: { screen: SkillsScreen },
   },
-});
+  {
+    tabBarOptions: {
+      style: BaseStyleSheet.tabBar,
+    },
+  },
+);
 
 export default () => (
-    <Provider store={store}>
-      <App />
-    </Provider> 
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
