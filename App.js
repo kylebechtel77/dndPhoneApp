232160cf3f00e6552107a3a1275da61ce6fe5bb5
Timothy Sassone <timothy.s.dev@gmail.com>
2017-08-10 06:45:43 -0400

    Added file for equipment page (copy of ability screen for now), and added it to navigation.

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import BaseStyleSheet from './Styles/Base';
 import GeneralInfo from './Screens/GeneralInfo';
 import AbilitiesScreen from './Screens/Abilities';
 import SkillsScreen from './Screens/Skills';
+import EquipmentScreen from './Screens/Equipment';
 import dndApp from './Reducers/Reducer';
 
 const store = createStore(dndApp);
@@ -16,6 +17,7 @@ const App = TabNavigator(
     'General Info': { screen: GeneralInfo },
     Abilities: { screen: AbilitiesScreen },
     Skills: { screen: SkillsScreen },
+    Equipment: { screen: EquipmentScreen },
   },
   {
     tabBarOptions: {
---
+++ Screens/Equipment.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, ScrollView, Image, Dimensions } from 'react-native';
+import { connect } from 'react-redux';
+
+import AbilityRow from '../Components/AbilityRow';
+import BaseStyleSheet from '../Styles/Base';
+import Layout from '../Components/Layout';
+
+const image = require('../Images/BackgroundPortrait.png');
+
+const window = Dimensions.get('window');
+
+const EquipmentScreen = (props) => {
+  const { character } = props;
+  return (
+    <Image source={image} style={{ width: window.width, height: window.height }}>
+      <ScrollView>
+        <View style={BaseStyleSheet.card}>
+          <AbilityRow
+            ability={character.abilities.str}
+            proficiencyBonus={character.proficiencyBonus}
+          />
+          <Layout.RowDivider />
+          <AbilityRow
+            ability={character.abilities.dex}
+            proficiencyBonus={character.proficiencyBonus}
+          />
+          <Layout.RowDivider />
+          <AbilityRow
+            ability={character.abilities.con}
+            proficiencyBonus={character.proficiencyBonus}
+          />
+          <Layout.RowDivider />
+          <AbilityRow
+            ability={character.abilities.int}
+            proficiencyBonus={character.proficiencyBonus}
+          />
+          <Layout.RowDivider />
+          <AbilityRow
+            ability={character.abilities.wis}
+            proficiencyBonus={character.proficiencyBonus}
+          />
+          <Layout.RowDivider />
+          <AbilityRow
+            ability={character.abilities.cha}
+            proficiencyBonus={character.proficiencyBonus}
+            last
+          />
+        </View>
+      </ScrollView>
+    </Image>
+  );
+};
+
+EquipmentScreen.propTypes = {
+  character: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+};
+
+const mapStateToProps = state => ({
+  character: state.characters[0],
+});
+
+export default connect(
+  mapStateToProps,
+)(EquipmentScreen);
